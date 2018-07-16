248fa67b48af9efabe6350bf510a9672a0684f10
Paul Carlo Tordecilla <ptordecilla@posible.net>
2018-07-16 12:07:43 +0800

    remove pusher.js

--- resources/views/student/ajaxdata.blade.php
+++ resources/views/student/ajaxdata.blade.php
@@ -62,8 +62,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
-    <script src="https://js.pusher.com/4.1/pusher.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.2/axios.js"></script>
+
 <script>
 	$(document).ready(function(){
 		//View Database
